ffe97613febf46036f634cb8d680f1ee8bfc3a10
lg017 <laura.gutierrez.osorno@gmail.com>
2025-10-22 23:58:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,6 @@ client1.on_message = on_message
 
 
 st.title("INTERFACES MULTIMODALES")
-st.markadown("<h1 style='text-align: center; color: green;'>Interfaces multimodales</h1>", unsafe_allow_html=True)
 st.subheader("CONTROL POR VOZ")
 
 image = Image.open('voice_ctrl.jpg')
